588e81a7c8d3215d19125f1a2fbc7b13593bdf4b
manoelehmann <125097266+manoelehmann@users.noreply.github.com>
2023-03-13 20:28:42 -0300

    Add files via upload

---
+++ quadrado.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float n1,n2, resultado;
+	
+	cout<<"Informe um valor real: ";
+	cin>>n1;
+	
+	cout<<"Informe outro valor real: ";
+	cin>>n2;
+	
+	resultado=(n1*n1)+(n2*n2);
+	cout<<"A soma dos quadrados e: "<<resultado<<endl;
+	
+	
+}
